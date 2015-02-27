6cce2d7c549eaf816fc75761c384f19b361d36d5
azy2 <lambethben@gmail.com>
2015-02-27 07:07:15 -0600

    fixed html and boidling.js

--- js/boidling.js
+++ js/boidling.js
@@ -1,5 +1,5 @@
 Boidling = function () {
-    this.geometry = new THREE.CylinderGeometry(
+    var geometry = new THREE.CylinderGeometry(
         0, // radiusTop
         20, // radiusBottom
         100, // height
@@ -8,22 +8,9 @@ Boidling = function () {
         false // openEnded
     );
 
-    this.posx = Math.floor((Math.random() * 10) + 1);
-    this.posy = Math.floor((Math.random() * 10) + 1);
-    this.posz = Math.floor((Math.random() * 10) + 1);
-
-    this.geometry.position.x = this.posx;
-    this.geometry.position.y = this.posy;
-    this.geometry.position.z = this.posz;
-
-    this.material = new THREE.MeshBasicMaterial( {color: 0x00ffff, wireframe: true} );
+    var material = new THREE.MeshBasicMaterial( {color: 0x00ffff, wireframe: true} );
 
     this.mesh = new THREE.Mesh(geometry, material);
-
-    this.num1 = Math.floor((Math.random() * 10) + 1);
-    this.num2 = Math.floor((Math.random() * 10) + 1);
-    this.num3 = Math.floor((Math.random() * 10) + 1);
-    this.velocity = new THREE.Vector3(num1, num2, num3);
 
     this.update = function() {
         //geometry.position.x += velocity.x
